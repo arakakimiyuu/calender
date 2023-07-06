a08203fe95377797218e9ddcd02c3a083042141c
EC2 Default User <kaeru.1210my@gmail.com>
2023-07-06 07:48:46 +0000

    [Add]showページ編集

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,32 +1,39 @@
 class BlogsController < ApplicationController
-  
+
+  def index
+    @blogs = Blog.all
+    @blog = Blog.new
+  end
+
   def new
     @blog = Blog.new
   end
-  
-  def create
-  end
-  
-  def index
-    @blogs = Blog.a
-  end
-  
+
   def show
+    @blog = Blog.find(params[:id])
   end
-  
-  def edit
+
+  def create
+    Blog.create(blog_params)
+    redirect_to blogs_path
   end
-  
-  def update
-  end
-  
+
   def destroy
+
   end
-  
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
   private
-  
+
   def blog_params
     params.require(:blog).permit(:title, :content, :start_time)
   end
-  
+
 end
